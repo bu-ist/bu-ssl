bfd20583a838f501b721ea3b4e29efc271be0cf9
Carlos E Silva <cedas@bu.edu>
2017-11-08 11:07:22 -0500

    Run phpcbf with the WordPress standard on the inc folder

--- inc/wpcli.php
+++ inc/wpcli.php
@@ -1,67 +1,67 @@
 <?php
 
 class SSL_CLI extends WP_CLI_Command {
-    public $ssl;
+	public $ssl;
 
-    function __construct() {
-        $this->ssl = new \BU\WordPress\Plugins\SSL();
-    }
+	function __construct() {
+		$this->ssl = new \BU\WordPress\Plugins\SSL();
+	}
 
-    /**
-     * Find insecure images & update postmeta
-     * 
-     * ## OPTIONS
-     * 
-     * <site_id> : ID of the site 
-     * 
-     * ## EXAMPLES
-     * 
-     *     wp bu-ssl findimages --ssldebug
-     *
-     * @synopsis [--ssldebug]
-     */
-    function findimages( $args, $assoc_args ) {        
-        global $wpdb;
+	/**
+	 * Find insecure images & update postmeta
+	 *
+	 * ## OPTIONS
+	 *
+	 * <site_id> : ID of the site
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     wp bu-ssl findimages --ssldebug
+	 *
+	 * @synopsis [--ssldebug]
+	 */
+	function findimages( $args, $assoc_args ) {
+		global $wpdb;
 
-        $postids = get_posts( array(
-                'post_type'                 => array( 'any' ),
-                'nopaging'                  => true,
-                'cache_results'             => false,
-                'update_post_meta_cache'    => false,
-                'update_post_term_cache'    => false,
-                'fields'                    => 'ids',
-            ) );
-        
-        WP_CLI::log( sprintf( "Scanning site #%d: Checking %d posts", get_current_blog_id(), count( $postids ) ) );
+		$postids = get_posts( array(
+				'post_type'                 => array( 'any' ),
+				'nopaging'                  => true,
+				'cache_results'             => false,
+				'update_post_meta_cache'    => false,
+				'update_post_term_cache'    => false,
+				'fields'                    => 'ids',
+		) );
 
-        $this->ssl->remove_all_postmeta();
+		WP_CLI::log( sprintf( 'Scanning site #%d: Checking %d posts', get_current_blog_id(), count( $postids ) ) );
 
-        if ( $postids ) {
-            foreach ( $postids as $id ){ 
-                $post = get_post( $id );
+		$this->ssl->remove_all_postmeta();
 
-                $urls = $this->ssl->search_for_insecure_images_by_post( $id );
-                $debug = '';
+		if ( $postids ) {
+			foreach ( $postids as $id ) {
+				$post = get_post( $id );
 
-                if( isset( $assoc_args['ssldebug'] ) ){
-                    $debug = "\n" . var_export( $urls, true ) . "\n";
-                }
-                
-                $this->ssl->do_update_postmeta( $id, $urls );
-                
-                if( count( $urls ) ){
-                    WP_CLI::warning( sprintf( 
-                        "#%d '%s' - %d insecure image(s)", 
-                        $id, 
-                        $post->post_title,
-                        count( $urls ), 
-                        get_permalink( $post )
-                    ) . $debug );
-                }
-            }
-            WP_CLI::success( sprintf( "%d posts scanned.", count( $postids ) ) );
-        }
-    }
+				$urls = $this->ssl->search_for_insecure_images_by_post( $id );
+				$debug = '';
+
+				if ( isset( $assoc_args['ssldebug'] ) ) {
+					$debug = "\n" . var_export( $urls, true ) . "\n";
+				}
+
+				$this->ssl->do_update_postmeta( $id, $urls );
+
+				if ( count( $urls ) ) {
+					WP_CLI::warning( sprintf(
+						"#%d '%s' - %d insecure image(s)",
+						$id,
+						$post->post_title,
+						count( $urls ),
+						get_permalink( $post )
+					) . $debug );
+				}
+			}
+			WP_CLI::success( sprintf( '%d posts scanned.', count( $postids ) ) );
+		}
+	}
 }
 
 WP_CLI::add_command( 'bu-ssl', 'SSL_CLI' );
